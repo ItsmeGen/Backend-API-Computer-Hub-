72ab2ebc831ac8cb527c1b0091139ebb77ad0ad4
Gen <lirazangm@gmail.com>
2025-03-19 10:08:20 +0800

    Added fetch_filtered_orders and remove fetch_orders.php

--- filter_orders/fetch_filtered_orders.php
+++ filter_orders/fetch_filtered_orders.php
@@ -2,9 +2,9 @@
 header("Content-Type: application/json");
 include '../productApi/db_conn.php';
 
-
-if (isset($_GET['user_id'])) {
-    $user_id = $_GET['user_id']; 
+if (isset($_GET['user_id']) && isset($_GET['order_status'])) {
+    $user_id = $_GET['user_id'];
+    $order_status = $_GET['order_status'];
 
     $sql = "SELECT orders.id, orders.user_id, orders.customer_name, orders.customer_phone, orders.customer_address,
            orders.order_status, orders.created_at, order_items.product_name, order_items.quantity, order_items.price,
@@ -12,10 +12,10 @@ if (isset($_GET['user_id'])) {
     FROM orders
     INNER JOIN order_items ON orders.id = order_items.order_id
     INNER JOIN products ON order_items.product_id = products.product_id
-    WHERE orders.user_id = ?"; 
+    WHERE orders.user_id = ? AND orders.order_status = ?";
 
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $user_id); 
+    $stmt->bind_param("is", $user_id, $order_status);
 
     if ($stmt->execute()) {
         $result = $stmt->get_result();
@@ -32,9 +32,8 @@ if (isset($_GET['user_id'])) {
 
     $stmt->close();
 } else {
-    echo json_encode(["error" => "User ID not provided"]);
+    echo json_encode(["error" => "User ID and Order Status must be provided"]);
 }
 
 $conn->close();
-
 ?>
